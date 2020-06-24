2412cfd08671e26117783d9e0dfbb073518cc2f1
Vijeesh1990 <vnvijeeshvn@gmail.com>
2020-06-24 11:29:23 +0530

    Pushing Calculator.java and TestCalculator.java

---
+++ MyApp/src/main/java/com/ofss/Calculator.java
@@ -0,0 +1,7 @@
+package com.ofss;
+
+public class Calculator {
+
+	
+	
+}
---
+++ MyApp/src/main/java/com/ofss/TestCalculator.java
@@ -0,0 +1,11 @@
+package com.ofss;
+
+public class TestCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator c = new Calculator();
+		System.out.println("New Calculator created");
+	}
+
+}
